1b008e35e0b41facae9bf20171be302d7d9b76ca
Morgan McCauley <morgan.mccauley94@gmail.com>
2018-10-02 22:02:08 +1300

    initial

---
+++ App/Components/Exercise/Exercise.spec.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import Exercise from './Exercise';
+
+describe('<Exercise />', () => {
+  it('should render', () => {
+    // const wrapper = shallow(<Exercise name="" sets={[]} />);
+    // console.log(wrapper);
+  });
+});
---
+++ test/setup.js
@@ -0,0 +1,7 @@
+var chai = require("chai");
+var sinon = require('sinon');
+
+// require('react-native-mock/mock');
+
+global.sinon = sinon;
+global.expect = chai.expect;
